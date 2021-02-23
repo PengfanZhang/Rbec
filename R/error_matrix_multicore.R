bf4e09b6e714cdb57e0889bdb460408d99a19a59
PengfanZhang <zhangpengfan@PengfanZhangs-MacBook-Pro.local>
2021-02-23 14:22:12 +0100

    First commit

---
+++ R/error_matrix_multicore.R
@@ -0,0 +1,101 @@
+#' Reference-based error correction of amplicon sequencing data
+#'
+#' @description
+#' This function calculate the error matrix
+#'
+#' @details Ruben Garrido-Oter's group, Plant-Microbe interaction, Max Planck Institute for Plant Breeding Research
+#' @author  Pengfan Zhang
+#'
+#' @param fq the path of merged amplicon sequencing reads in fastq format (Ns are not allowed in the reads)
+#' @param ref the unique reference sequences of the reference seqeunces, each sequence must be in one line (Ns are not allowed in the sequences)
+#' @param sample_size the sampling size of reads to generate the transition matrix
+#' @param threads the number of threads used to align the query reads to reference sequences
+#' @param ascii ascii characters used to encode phred scores
+#'
+#'
+#' @import dada2
+#' @import foreach
+#' @import doParallel
+#'
+#' @usage error_m(fq, ref, sample_size, threads, ascii)
+#' @examples
+#'
+#' @return The output is a 20 by 43 transition probability matrix
+#'
+#' @export
+#'
+
+error_m <- function(fq, ref, sample_size=10000, threads, ascii) {
+
+    t1 <- Sys.time()
+
+    # read FASTQ file
+    if (grepl(".gz", fq, fixed = T)) {
+        raw_data <- read_lines(gzfile(fq))
+    }
+    else {
+        raw_data <- read_lines(fq)
+    }
+    if (sample_size > length(raw_data)/4){
+      stop("The sampling size ", sample_size ," exceeds the total number of reads ", length(raw_data)/4 ," in the input file")
+    }
+
+    # calculate the initial abundance of reference sequences
+    derep1 <- derepFastq(fq)
+    ref$abd <- derep1[["uniques"]][match(ref$ref_seq, names(derep1[["uniques"]]))]
+    ref <- ref[!is.na(ref$abd), ]
+    ref$est_abd <- 0
+
+    # find the reference sequences with highest similarities for each unique sequence
+    uniseqs <- names(derep1[["uniques"]])
+
+    # TODO: define this function outside
+    findbest <- function(que) {
+        # use the kmer distance function implemented in DADA2
+        kdist <- function(que, ref, kmer=7) {
+            dis <- kmer_dist(que, ref, kmer)
+            return(dis)
+        }
+        # calculate k-mer distances for all sequences
+        kdist_res <- unlist(lapply(ref$ref_seq, function(x) kdist(que, x)))
+        best_match <- which.min(kdist_res)
+        # if there are more than 1 hits showing the same maximum identity,
+        # the reference with the highest abundance is chosen as the best reference sequence
+        if (length(best_match) > 1) {
+          ref_candidate <- ref[best_match, ]
+          ref_best <- ref_candidate[which.max(ref_candidate[, 2]), 1]
+        }
+        else {
+          ref_best <- ref[best_match, 1]
+        }
+        return(c(que, ref_best))
+    }
+
+    #cl <- makeCluster(threads)
+    registerDoParallel(cores=threads)
+    que2ref <- foreach::foreach(x=uniseqs, .combine='rbind') %dopar% findbest(x)
+    stopImplicitCluster()
+    que2ref <- as.character(que2ref[, 2])
+    print("Finished finding the best reference sequences for each unique sequences.")
+    t2 <- Sys.time()
+    print(t2-t1)
+
+    # sample raw sequences for transition matrix generation
+    sample_label <- sample(1:(length(raw_data)/4), sample_size)
+    seqs <- raw_data[sample_label*4-2]
+    seqs <- toupper(seqs)
+    qual <- raw_data[sample_label*4]
+    bestref <- que2ref[derep1[["map"]][sample_label]]
+    query <- data.frame(seqs=seqs, qual=qual, refs=bestref)
+
+    # calculate the transition matrix and error matrix
+    trans_matrix <- trans_m(query, ascii)
+    error_matrix <- loessErr(trans_matrix)
+
+    derep1[["bestref"]] <- que2ref
+
+    error_ref_matrix <- list(ref=ref, err=error_matrix, derep=derep1, total_reads=length(raw_data)/4)
+    return(error_ref_matrix)
+
+}
+
---
+++ R/transition_matrix.R
@@ -0,0 +1,62 @@
+
+#' Reference-based error correction of amplicon sequencing data
+#'
+#' @description
+#' This function count the transition matrix
+#'
+#' @details Ruben Garrido-Oter's group, Plant-Microbe interaction, Max Planck Institute for Plant Breeding Research
+#' @author Pengfan Zhang
+#'
+#' @param query list containing subsampled amplicon sequencing reads, quality scores, and reference sequences showing the highest identity for each read (Ns are not allowed in the reads)
+#' @param ascii ascii characters used to encode phred scores
+#'
+#' @usage trans_m(query, ascii)
+#' @examples
+#'
+#' @return The output is a 20 by 43 matrix containing the counts for different kinds of transitions
+#'
+#' @export
+#'
+
+trans_m <- function(query, ascii) {
+
+    # initialize the transition matrix
+    trans_matrix <- matrix(0, ncol=43, nrow=20)
+    trans_kinds <- c(paste(rep(c("A","T","G","C","-"), each=4),
+                           rep(2, 20), rep(c("A","T","G","C"), 5), sep=""))
+    rownames(trans_matrix) <- trans_kinds
+    colnames(trans_matrix) <- as.character(seq(0, 42))
+
+    # TODO: define outside
+    tm_cal <- function(que, qual, ref){
+
+    # perform global alignment between query and reference using NW
+    align_out <- nwalign(que, ref)
+    Seq1align <- align_out[1]
+    Seq2align <- align_out[2]
+    Seq1align <- unlist(strsplit(Seq1align, ""))
+    Seq2align <- unlist(strsplit(Seq2align, ""))
+    align_pairs <- paste(Seq2align, Seq1align, sep="2")
+    align_pairs <- align_pairs[!endsWith(align_pairs, "-")]
+
+    # check if the quality system is phred 33 or 64
+    qual <- as.character(strtoi(charToRaw(paste((qual), collapse="")), 16L)-ascii)
+
+    align_data <- data.frame(alig=align_pairs, qual=qual, stringsAsFactors=F)
+    return(align_data)
+
+    }
+
+    # for all query sequences, calculate global alignments to reference sequence and corresponding transitions
+    align_data <- do.call(rbind, apply(query, 1, function(x) tm_cal(x[1], x[2], x[3])))
+
+    # transform transitions for each aligned querity to transition matrix
+    align_data <- data.frame(table(align_data), stringsAsFactors=F)
+    for(i in 1:nrow(align_data)){
+      trans_matrix[as.character(align_data[i, 1]), as.character(align_data[i, 2])] <- align_data[i, 3]
+    }
+
+    return(trans_matrix)
+
+}
+
